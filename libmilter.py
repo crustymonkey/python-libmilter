3b3a8987296f5745ec162d345c2a88e34260743f
danBLA <danBLA@users.noreply.github.com>
2018-08-06 23:53:13 +0200

    import print_function from __future__ to have printing to stderr in py2/3

--- libmilter.py
+++ libmilter.py
@@ -14,7 +14,7 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with python-libmilter.  If not, see <http://www.gnu.org/licenses/>.
-
+from __future__ import print_function
 import struct , sys , select , threading , socket , time , os , signal
 
 # Turn debugging on or off
